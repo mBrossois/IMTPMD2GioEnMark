f1be258390e4a8c5a0142da5de7bb226f4e57172
Giovski <Giovski>
2016-04-01 15:15:05 +0200

    Persoons scherm ingevoegd en tevens gezorgd dat de naam kunnen worden ingevoerd
    Persoons scherm ingevoerd

--- MyApplication/app/src/main/java/com/example/gioenmark/myapplication/Persoonsscherm.java
+++ MyApplication/app/src/main/java/com/example/gioenmark/myapplication/Persoonsscherm.java
@@ -2,6 +2,7 @@ package com.example.gioenmark.myapplication;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
@@ -15,23 +16,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class Persoonsscherm extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_persoonsscherm);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        SharedPreferences preferences2 = PreferenceManager.getDefaultSharedPreferences(this);
-        String name = preferences2.getString("Name", "");
-
-        TextView textViewToChange = (TextView) findViewById(R.id.textView2);
-        textViewToChange.setText(name);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -50,7 +57,15 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        navigationView.getMenu().getItem(0).setChecked(true);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        SharedPreferences preferences2 = PreferenceManager.getDefaultSharedPreferences(this);
+        String name = preferences2.getString("Name", "");
+
+        EditText textViewToChange = (EditText) findViewById(R.id.editText);
+        textViewToChange.setText(name);
     }
 
     @Override
@@ -66,7 +81,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.persoonsscherm, menu);
         return true;
     }
 
@@ -92,6 +107,8 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
             // Handle the camera action
 
         } else if (id == R.id.nav_gallery) {
@@ -106,17 +123,67 @@ public class MainActivity extends AppCompatActivity
             Intent intent = new Intent(this, OverzichtScherm.class);
             startActivity(intent);
         } else if (id == R.id.nav_share) {
-            Intent intent = new Intent(this, Persoonsscherm.class);
-            startActivity(intent);
-
-/*
-        } else if (id == R.id.nav_send) {
-*/
 
         }
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-
         return true;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Persoonsscherm Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.gioenmark.myapplication/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Persoonsscherm Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.gioenmark.myapplication/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    public void veranderNaam(View view)
+    {
+        EditText edittext = (EditText) findViewById(R.id.editText);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("Name",edittext.getText().toString());
+        editor.apply();
+
+        SharedPreferences preferences2 = PreferenceManager.getDefaultSharedPreferences(this);
+        String name = preferences2.getString("Name", "");
+
+        EditText textViewToChange = (EditText) findViewById(R.id.editText);
+        textViewToChange.setText(name);
+    }
 }
+
