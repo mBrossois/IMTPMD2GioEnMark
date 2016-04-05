45efcc0f87640909fffab77b6cbd3497daa39c53
mBrossois <mark.dnb@live.nl>
2016-04-05 16:29:06 +0200

    Added GsonRequest, VolleyHelper, Course and specification for JSON
    Added the above called classes. In Manifest and the gradle files te
    permission and needed depencies and repositories. In the invoerScherm
    their is also hard code added to read a JSonString.

--- MyApplication/app/src/main/java/com/example/gioenmark/myapplication/InvoerScherm.java
+++ MyApplication/app/src/main/java/com/example/gioenmark/myapplication/InvoerScherm.java
@@ -13,10 +13,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.example.gioenmark.myapplication.GSON.GsonRequest;
+import com.example.gioenmark.myapplication.Models.Course;
+import com.example.gioenmark.myapplication.VOLLEYHELPER.VolleyHelper;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InvoerScherm extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    //String courses;
+     Course[] courses;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,14 +48,25 @@ public class InvoerScherm extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        String json = "[{name:'Progr.Mob.Devs', ects:3, code:’IMTPMD’, grade:6}," +
+                "{name: 'MT PROJECT', ects: 6, code: ‘IPOMED3’, grade:6}]";
+
+        Gson gson = new Gson();		// Dependency in gradle - see next slide
+
+        courses = gson.fromJson(json, Course[].class);	// CONVERT JSON
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Hardcoded From JSON"+courses[0].grade, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+        Snackbar.make(this.findViewById(android.R.id.content), "Hardcoded From JSON"+courses[0].ects, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -106,4 +141,30 @@ public class InvoerScherm extends AppCompatActivity
 
         return true;
     }
+
+    public void grabJson(View view)
+    {
+        requestSubjects();
+    }
+
+    private void requestSubjects(){
+        Type type = new TypeToken<List<Course>>(){}.getType();
+        GsonRequest<List<Course>> request = new GsonRequest<List<Course>>(
+                "http://fuujokan.nl/subject_lijst.json", type, null,
+                new Response.Listener<List<Course>>() {
+                    @Override
+                    public void onResponse(List<Course> response) { processRequestSucces(response); }
+                }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error){ processRequestError(error);     }
+        }
+        );
+        VolleyHelper.getInstance(this).addToRequestQueue(request);
+//        courses = Course;
+    }
+
+    private void processRequestSucces(List<Course> subjects ){   }
+
+    private void processRequestError(VolleyError error){  }
+
 }
